a2eb0b4b62207f6b11b04af101d2d00b7704dd0a
RugaCoder <fruganyumisa@gmail.com>
2020-07-13 12:46:29 +0300

    updated deploy sh

--- node/node-deploy.sh
+++ node/node-deploy.sh
@@ -6,24 +6,24 @@
 
 echo "Welcome to our node deployment assistant \n "
 
-echo "Please enter the git url for your project using https or ssh if you use ssh make sure ssh keys exist"
+echo "Please enter the git url for your project using https or ssh if you use ssh make sure ssh keys exist\n"
 read giturl
 
-echo "Please enter the name  for  your node project"
+echo "Please enter the name  for  your node project\n"
 
 read projectname
 
+echo "Installation directory where your project will be installed"
+
+read projectpath
+
+cd $projectpath
+sleep 1
+
 git clone $giturl $projectname 
 cd $projectname
+ npm install pm2@latest -g
+ yarn
+ yarn build
+ pm2 start npm --name $projectname --log /var/log/portal.log
 
-currentdir = pwd
-
-if $currentdir = $projectname
-then
-    npm install pm2@latest -g
-    yarn
-    yarn build
-    pm2 start npm --name $projectname --log /var/log/portal.log
-
-
-fi 
